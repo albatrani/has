6cdc90586a18b4900aefad00eb4bc6bd753a9172
abid1403 <abid.d2@gmail.com>
2015-10-20 11:51:02 -0400

    finished LightingControlAction

--- Team4Iteration1/src/LightingControlAction.java
+++ Team4Iteration1/src/LightingControlAction.java
@@ -1,4 +1,15 @@
 
 public class LightingControlAction {
+	private LightStatus action;
+	private HouseSection section;
+	public LightingControlAction(HouseSection hs,LightStatus action) {
+		section = hs;
+		this.action = action;
+	}
+	public boolean process() {
+		
+		section.setLightingStatus(action);
+		return section.getLightingStatus() == action;
 
+	}
 }
