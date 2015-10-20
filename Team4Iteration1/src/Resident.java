039a706cc82ce1ebf7fa0263c82840bd891029c7
abid1403 <abid.d2@gmail.com>
2015-10-20 12:38:01 -0400

    finished Resident

--- Team4Iteration1/src/Resident.java
+++ Team4Iteration1/src/Resident.java
@@ -1,4 +1,23 @@
 
 public class Resident {
 // get and set
+	private String name;
+	private String password;
+	public Resident(String name, String password) {
+		this.name = name;
+		this.password = password;
+		
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
