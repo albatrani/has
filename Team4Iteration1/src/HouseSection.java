1fde99c3744a573ab72b93f2659f8d3d07ed7e9e
Khaled <Khaled@163.118.245.71>
2015-10-20 11:39:49 -0400

    Finished HouseSection

--- Team4Iteration1/src/HouseSection.java
+++ Team4Iteration1/src/HouseSection.java
@@ -1,4 +1,40 @@
 
 public class HouseSection {
+	
+	private String name;
+	private boolean inHouse = true;
+	private LightAdapter lightAdapter = new LightAdapter();
+	
+	public void setLightingStatus(LightStatus action){
+		
+		switch(action){
+		case On:
+			lightAdapter.on();
+		case Off:
+			lightAdapter.off();
+		}
+	}
+
+	public LightStatus getLightingStatus() {
+		
+		return lightAdapter.getLightStatus();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isInHouse() {
+		return inHouse;
+	}
+
+	public void setInHouse(boolean inHouse) {
+		this.inHouse = inHouse;
+	}
+	
 
 }
