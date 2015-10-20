07ee4c8284b20cb8cd0d0486d8b9d715c9a46847
Khaled <Khaled@163.118.245.71>
2015-10-20 12:50:12 -0400

    Controling Objects

--- Team4Iteration1/src/ControlLightingHandler.java
+++ Team4Iteration1/src/ControlLightingHandler.java
@@ -1,4 +1,31 @@
 
 public class ControlLightingHandler {
+	
+	House house;
+	LightingControlTransaction lct;
+	public ControlLightingHandler(House house) {
+		// TODO Auto-generated constructor stub
+		this.house = house;
+		lct = new LightingControlTransaction();
+	}
+	
+	public HouseSection selectHousesection(String name){
+		
+		
+		return house.getHouseSection(name);
+	}
+	
+	public void turnOnOffLighting(HouseSection hs, LightStatus action){
+		
+		lct.addLightControlAction(hs, action);
+	}
+	
+	public TransactionStatus endControlLighting(){
+		
+		TransactionStatus status = lct.complete();
+		house.logTransaction(lct, status);
+		
+		return status;
+	}
 
 }
--- Team4Iteration1/src/House.java
+++ Team4Iteration1/src/House.java
@@ -1,4 +1,77 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class House {
+	
+	private String address;
+	private Resident resident;
+	private List<TransactionLog> trasactions;
+	private Map<String, HouseSection> sections;
+	
+	public House(Resident resident, String address) {
+		// TODO Auto-generated constructor stub
+		
+		this.resident = resident;
+		this.address = address;
+		trasactions = new ArrayList<>();
+		sections = new HashMap<>();
+		
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+
+	public Resident getResident() {
+		return resident;
+	}
+
+	public void setResident(Resident resident) {
+		this.resident = resident;
+	}
+
+	public List<TransactionLog> getTrasactions() {
+		return trasactions;
+	}
+
+	public void setTrasactions(List<TransactionLog> trasactions) {
+		this.trasactions = trasactions;
+	}
+
+	public Map<String, HouseSection> getSections() {
+		return sections;
+	}
+
+	public void setSections(Map<String, HouseSection> sections) {
+		this.sections = sections;
+	}
+
+	public HouseSection getHouseSection(String name) {
+		// TODO Auto-generated method stub
+		
+		return sections.get(name);
+	}
+
+	public void logTransaction(LightingControlTransaction lct, TransactionStatus status) {
+		// TODO Auto-generated method stub
+		
+		trasactions.add(new TransactionLog(lct, status));
+	}
+	
+	public void addHouseSection(HouseSection hs){
+		
+		if(!sections.containsKey(hs.getName())){
+			sections.put(hs.getName(), hs);
+		}
+			
+	}
 
 }
