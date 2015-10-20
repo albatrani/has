ed6479311a5ce73370b3c9155bdc21f7ef3cd786
abid1403 <abid.d2@gmail.com>
2015-10-20 12:20:08 -0400

    finished TransactionLog
    Adding new Class TransactionType

--- Team4Iteration1/src/TransactionLog.java
+++ Team4Iteration1/src/TransactionLog.java
@@ -1,4 +1,16 @@
+import java.util.Date;
 
 public class TransactionLog {
-
+	private TransactionType type;
+	private Date dateTime;
+	private TransactionStatus status;
+	private LightingControlTransaction transaction;
+	public TransactionLog(LightingControlTransaction lct, TransactionStatus status) {
+		transaction = lct;
+		dateTime = new Date(System.currentTimeMillis());
+		type= TransactionType.controlLighting;
+		this.status = status;
+		
+	}
+	
 }
