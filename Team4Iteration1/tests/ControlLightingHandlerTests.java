f23b02939d73406948f7f7e7f7fd50aece3d3182
Khaled <Khaled@163.118.245.95>
2015-10-21 19:47:21 -0400

    Adding controlLighthandlertests

---
+++ Team4Iteration1/tests/ControlLightingHandlerTests.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ControlLightingHandlerTests {
+	
+	private ControlLightingHandler handler;
+
+	@Before
+	public void setUp() throws Exception {
+		
+		handler = new ControlLightingHandler();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testControlLightingHandler() {
+		
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectHousesection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTurnOnOffLighting() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEndControlLighting() {
+		fail("Not yet implemented");
+	}
+
+}
--- Team4Iteration1/tests/TestingUtilities.java
+++ Team4Iteration1/tests/TestingUtilities.java
@@ -1,12 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestingUtilities {
 
-    public final static String[] SECTIONS_NAMES = {
-            "Front Door", "Living Room", "Master Bedroom", "Master Bedroom Bath",
-            "Master Bedroom Closet", "Kitchen", "Stairway", "Upstair Looby",
-            "Kids Bedroom", "Guest Bedroom", "Guest Bathroom"
-    };
-    public final static boolean[] SECTIONS_IN_HOUSE_FLAGS = {
-            false, true, true, true, true, true, true, true, true, true, true,
-    };
+	public final static String[] SECTIONS_NAMES = {
+			"Front Door", "Living Room", "Master Bedroom", "Master Bedroom Bath",
+			"Master Bedroom Closet", "Kitchen", "Stairway", "Upstair Looby",
+			"Kids Bedroom", "Guest Bedroom", "Guest Bathroom"
+	};
+	public final static boolean[] SECTIONS_IN_HOUSE_FLAGS = {
+			false, true, true, true, true, true, true, true, true, true, true,
+	};
 
+	public static House getSampleHouse(){
+
+		House house = new House(new Resident("Said", "abc"));
+	}
+
+public static List<HouseSection> getHousesections(){
+	// Act
+	List<HouseSection> list = new ArrayList<>();
+    for (int i = 0; i < TestingUtilities.SECTIONS_NAMES.length; i++) {
+        HouseSection hs =
+                new HouseSection(TestingUtilities.SECTIONS_NAMES[i],
+                        TestingUtilities.SECTIONS_IN_HOUSE_FLAGS[i]);
+        list.add(index, element);
+        lct.addLightControlAction(hs, action);
+    }
+}
 }
